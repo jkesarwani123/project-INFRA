923c16a7cff7e0045851004d323613666c3bdd3e
Juhi K <jkesarwani.us@gmail.com>
2023-05-31 13:21:49 -0400

    module-subnets

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "vpc" {
         for_each = var.vpc
         cidr_block = each.value["cidr_block"]
         subnets = each.value["subnets"]
-
         tags = local.tags
         env = var.env
 }
