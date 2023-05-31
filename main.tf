5eda98093740841313c05bb49a7b7f3680d92ed2
Juhi K <jkesarwani.us@gmail.com>
2023-05-30 23:05:04 -0400

    module-VPC

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
         source = "git::https://github.com/jkesarwani123/module-infra-vpc.git"
         for_each = var.vpc
         cidr_block = each.value["cidr_block"]
+        subnets = each.value["subnets"]
         tags = local.tags
         env = var.env
 }
