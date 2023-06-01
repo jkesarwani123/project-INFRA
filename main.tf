0d168f7ac682e625fc29b3691eee56687bec346b
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 09:10:56 -0400

    App server instances

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ output "test" {
         value = module.vpc
 }
 
-#module "app" {
-#        source   = "git::https://github.com/jkesarwani123/module-infra-app.git"
-#        for_each = var.app
-#        instance_type = each.value["instance_type"]
-#        subnet_id = element(lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null), "subnet_ids", null),0)
-#
-#}
+module "app" {
+        source   = "git::https://github.com/jkesarwani123/module-infra-app.git"
+        for_each = var.app
+        instance_type = each.value["instance_type"]
+        subnet_id = element(lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null), "subnet_ids", null),0)
+
+}
