b79a667f39e139c8156e7693511e31c7daf9d778
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 15:05:55 -0400

    AutoScaling grp

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "app" {
         min_size = each.value["min_size"]
         env=var.env
         bastion_cidr=var.bastion_cidr
+        tags= local.tags
         vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
         subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null), "subnet_ids", null)
         allow_app_cidr=lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null), "subnet_cidrs", null)
