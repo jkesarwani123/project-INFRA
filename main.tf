8ca7970408807f89d7d677c509ae8bcc76a2ce7d
Juhi K <jkesarwani.us@gmail.com>
2023-05-31 23:18:28 -0400

    App server instances

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ module "app" {
         source   = "git::https://github.com/jkesarwani123/module-infra-app.git"
         for_each = var.app
         instance_type = each.value["instance_type"]
-        subnet_id = element(lookup(lookup(lookup(module.vpc,"main",null),"name",null), "subnet_ids", null),0)
+        subnet_id = element(lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null), "subnet_ids", null),0)
 
 }
