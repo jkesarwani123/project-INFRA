e0d0399419874fed90a8e51bb3cab6d53d5c8c71
Juhi K <jkesarwani.us@gmail.com>
2023-06-04 22:16:21 -0400

    App Module Target Grps

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "app" {
         desired_capacity = each.value["desired_capacity"]
         max_size = each.value["max_size"]
         min_size = each.value["min_size"]
-
+        app_port = each.value["app_port"]
         vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
         subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null), "subnet_ids", null)
         allow_app_cidr=lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null), "subnet_cidrs", null)
